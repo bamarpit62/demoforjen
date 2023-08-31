249f7d6df74f800b523957c8b51fe48fc9f6cf6a
bamarpit62 <bamniyaarpit@gmail.com>
2023-08-31 15:16:49 +0530

    first commit1

---
+++ src/test/java/com/demo/amdocsb5/Amdocsb5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.amdocsb5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Amdocsb5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
